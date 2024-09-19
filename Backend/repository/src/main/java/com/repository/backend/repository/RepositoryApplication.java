efc720efc7df580983e52ddfff8f117d4565a38a
ellen samanta <ellen.samanta@outlook.com>
2024-09-19 13:25:03 -0300

    feat: criando projeto backend

---
+++ Backend/repository/src/main/java/com/repository/backend/repository/RepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.repository.backend.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoryApplication.class, args);
+	}
+
+}
